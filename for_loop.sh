74ae59b20efacf5091f50b68e760cb1b1d30011e
nankspn <nankspn@gmail.com>
2024-11-24 13:15:54 +0530

    adding if and for loop codes

---
+++ for_loop.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+set -x
+
+for color in brown red blue yellow green; do 
+echo ${color}
+done
---
+++ if_condition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# set -x
+
+echo $1 $2
+
+if [ $1 == 'geek' ] && [ $2 == 'naveen' ] ; then
+iecho "yes your command line parameter or argument is matching to 'GEEK NAVEEN' ";
+fi
